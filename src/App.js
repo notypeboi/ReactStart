4fa8a3e98bfb480c8d181c7529ba5919321977f0
notypeboi <goodvvi@naver.com>
2020-03-09 19:05:04 +0900

    2020-03-09

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MyName from "./Components/MyName";
+import Counter from "./Components/Counter";
+import PhoneForm from "./Components/PhoneForm";
+import PhoneInfoList from "./Components/PhoneInfoList";
+class App extends Component {
+  id = 2;
+  //id를 숫자로 정해주는게 아니라 이 state속 information의 length로 지정할순 없을까
+  state = {
+    information: [
+      {
+        id: 0,
+        name: "김민준",
+        phone: "010-0000-0000"
+      },
+      {
+        id: 1,
+        name: "홍길동",
+        phone: "010-0000-0001"
+      }
+    ]
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleCreate = data => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    });
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div>
+        <div>안녕하세요!</div>
+        <MyName />
+        <MyName name="리액트" />
+        <Counter />
+        <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList data={this.state.information} />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Components/PhoneForm.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+class PhoneForm extends Component {
+  state = {
+    name: "",
+    phone: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    //페이지 리로딩 방지
+    this.props.onCreate(this.state);
+    //상태값을 onCreate를 통해서 부모에게 전달
+    this.setState({
+      name: "",
+      phone: ""
+      //상태 초기화
+    });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          placeholder="이름"
+          value={this.state.name}
+          onChange={this.handleChange}
+          name="name"
+        />
+        <input
+          placeholder="전화번호"
+          value={this.state.phone}
+          onChange={this.handleChange}
+          name="phone"
+        />
+        <button type="submit"> 등록 </button>
+        <div>
+          {this.state.name} {this.state.phone}
+        </div>
+      </form>
+    );
+  }
+}
+
+export default PhoneForm;
