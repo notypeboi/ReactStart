fe135c8555b0c239afa22d99e75560d0cf2dcc28
notypeboi <goodvvi@naver.com>
2020-03-09 19:05:35 +0900

    Revert "2020-03-09"
    This reverts commit 4fa8a3e98bfb480c8d181c7529ba5919321977f0.

--- src/Components/MyName.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-const MyName = ({ name }) => {
-  return <div>안녕하세요 제 이름은 {name} 입니다.</div>;
-};
-MyName.defaultProps = {
-  name: "기본이름"
-};
-export default MyName;
